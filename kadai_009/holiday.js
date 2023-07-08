6b28247ecef5004a2a7c2a6d5363128dffbf4a4b
ももちぃ <137071727+You-Momoti@users.noreply.github.com>
2023-07-08 19:44:34 +0900

    kadai_009をアップロード

--- kadai_009/holiday.js
+++ kadai_009/holiday.js
@@ -1,5 +1,12 @@
 let holidays = ["正月", "成人の日", "建国記念日", "天皇誕生日", "春分の日", "昭和の日", "憲法記念日", "みどりの日", "こどもの日", "海の日", "山の日", "敬老の日", "秋分の日", "スポーツの日", "文化の日", "勤労感謝の日"]
 
-for (let i = 1; i <= 15; i++) {
+// 変数holidaysの値が0以外である間、変数numの値を出力し続ける
+for (let i = 0; i <= holidays.length; i++) {
   console.log(holidays[i]);
+}
+
+let i = 0;
+while (i < holidays.length) {
+  console.log(holidays[i]);
+  i++;
 }
